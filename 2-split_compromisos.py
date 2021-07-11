c4e0f7c228f9439e6cd356a1c48dfbe447c3dd6f
Felipe Álvarez <felipealvarez@letsta.lk>
2021-07-11 13:19:38 -0400

    Sacando los indices de propuestas repetidos

--- 2-split_compromisos.py
+++ 2-split_compromisos.py
@@ -23,7 +23,9 @@ def to_json(df, filename):
 
 
 if __name__ == '__main__':
+    df_proposals = pd.read_csv('propuestas.csv')
     df = pd.read_csv('compromisos.csv')
+    df = df.loc[df['id'].isin(df_proposals.id)].reset_index()
     train, validate, test = train_validate_test_split(df, train_percent=.7, validate_percent=.15)
     to_json(train, "data/compromisos_train.json")
     to_json(validate, "data/compromisos_validate.json")
